a8e9a9778caeed5471a43d97e3a96b9857ccb204
ibtisam-adil <ibtisamadil273@gmail.com>
2024-03-16 02:01:28 +0500

    added js linters

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
+// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
 
-const todoList = document.querySelector(".todo-list");
+const todoList = document.querySelector('.todo-list');
 
-let todo = [
+const todo = [
   {
-    description: "Demo Task-1",
+    description: 'Demo Task-1',
     iscompleted: false,
     index: 0,
   },
   {
-    description: "Demo Task-2",
+    description: 'Demo Task-2',
     iscompleted: false,
     index: 1,
   },
   {
-    description: "Demo Task-3",
+    description: 'Demo Task-3',
     iscompleted: false,
     index: 2,
   },
   {
-    description: "Demo Task-4",
+    description: 'Demo Task-4',
     iscompleted: false,
     index: 3,
   },
   {
-    description: "Demo Task-5",
+    description: 'Demo Task-5',
     iscompleted: false,
     index: 4,
   },
   {
-    description: "Demo Task-6",
+    description: 'Demo Task-6',
     iscompleted: false,
     index: 5,
   },
   {
-    description: "Demo Task-7",
+    description: 'Demo Task-7',
     iscompleted: false,
     index: 6,
   },
   {
-    description: "Demo Task-8",
+    description: 'Demo Task-8',
     iscompleted: false,
     index: 7,
   },
 
 ];
 
-todoList.innerHTML = todo.map((arr) => {
-    return `
+todoList.innerHTML = todo.map((arr) => `
         <div class="dynamic-wrapper">
         <div class="check-p-wrap">
             <input class="checkbox" type="checkbox">
@@ -57,16 +57,4 @@ todoList.innerHTML = todo.map((arr) => {
         <i class='bx bx-dots-vertical-rounded'></i>
         </div>
         <div class="underline"></div>
-        `
-}).join("");
-
-// function component() {
-//   const element = document.createElement('div');
-
-//   // Lodash, currently included via a script, is required for this line to work
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
+        `).join('');
